1f3bbf315a92260fe9d9da743f24046065b53a21
DINESHAERO <dineshvelan96@gmail.com>
2022-09-22 19:37:43 +0530

    java

---
+++ src/com/pack/Project.java
@@ -0,0 +1,22 @@
+package com.pack;
+
+public class Project {
+	public void userName() {
+		System.out.println("dineshaero");
+	}
+	
+	public void email() {
+		System.out.println("dineshaero@gmail.com");
+	}
+	
+	public void phone () {
+		System.out.println("7402419006");
+	}
+	
+	public static void main(String[] args) {
+		Project p = new Project();
+		p.email();
+		p.userName();
+		p.phone();
+	}
+}
